3cb79511cab4c7e7456b4e23d84e736129d15a0d
Ivy Banana <cnnorwood14@gmail.com>
2026-03-02 20:03:18 -0500

    Implemented help flag - Helpful message with details displayed when help flag argument provided - ANSI Color Codes to add readability (will migrate to separate file for reusage later) - Case-and-dash-insensitive

--- src/main.c
+++ src/main.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define RED   "\033[0;31m"
+#define GREEN     "\033[0;32m"
+#define RESET   "\033[0m"
+#define BLUE    "\033[0;34m"
+#define BOLD    "\033[1m"
 
 int main(int argc, char *argv[]) {
-    printf("%s", argv[1]);
+    if (argc < 2) {
+        printf("Not enough arguments flags provided. Use -help for more information.");
+        return -1;
+    } else if (argc > 2) {
+        printf("Too many argument flags provided. Use -help for more information.");
+        return -1;
+    }
+
+    char *arg = argv[1];
+    while (*arg == '-') arg++;
+    char *p = arg;
+    while (*p) {
+        *p = tolower(*p);
+        p++;
+    }
+    if (!strcmp(arg, "help") || !strcmp(arg, "h"))
+        printf(
+            GREEN "Welcome to Flower Compiler!\n\n" RESET
+
+            BLUE "Usage:\n" RESET
+            "\tflower\t[options] <filepath>\n\n"
+
+            BLUE "Options:\n" RESET
+            "\t-help, -h\tShows this help message\n"
+            "\t<filepath>\tSpecify the source code file to compile\n\n"
+
+            BLUE "Example:\n" RESET
+            "\tflower -help\n"
+            "\tflower main.flo\n\n"
+
+            BLUE "Tips:\n" RESET
+            " - You can use any number of dashes before a flag, e.g., ---help\n"
+            " - Flags are " BOLD "case-insensitive" RESET ": -HELP works too!\n"
+        );
+
+    // printf("%s", argv[1]);
 
     return 0;
 }
